d2be73a284fc45336f20031d0cb997a2b121c148
Eric-Delangle <polvu@hotmail.fr>
2019-11-04 14:44:37 +0100

    Création des entités,des controleurs et les cruds

---
+++ ericpeinture/src/Entity/Picture.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\PictureRepository")
+ */
+class Picture
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="App\Entity\Category", inversedBy="pictures")
+     */
+    private $medium;
+
+    public function __construct()
+    {
+        $this->medium = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Category[]
+     */
+    public function getMedium(): Collection
+    {
+        return $this->medium;
+    }
+
+    public function addMedium(Category $medium): self
+    {
+        if (!$this->medium->contains($medium)) {
+            $this->medium[] = $medium;
+        }
+
+        return $this;
+    }
+
+    public function removeMedium(Category $medium): self
+    {
+        if ($this->medium->contains($medium)) {
+            $this->medium->removeElement($medium);
+        }
+
+        return $this;
+    }
+}
